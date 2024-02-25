3a813c165e01fa6c69f4d99f8f9c95c5fb3ebe2a
ronaldboodram <ronaldboodram@gmail.com>
2024-02-25 16:04:02 -0500

    modularize code and added spanner database operations

--- helper_classes/spanner_operations.py
+++ helper_classes/spanner_operations.py
@@ -3,12 +3,11 @@ import logging
 import os
 import time
 from dotenv import load_dotenv
-
 from helper_classes.sp_table import SpannerTable
 from helper_classes.spanner_columns import SpannerColumns
 from google.cloud import spanner
 from multiprocessing.dummy import Pool as ThreadPool
-from google.cloud.spanner_v1 import database
+# from google.cloud.spanner_v1 import database
 
 
 class SpannerOperations:
@@ -26,49 +25,34 @@ class SpannerOperations:
         # Read file into a list and close the file
         listings = rew_data.readlines()
         rew_data.close()
-
         # Load the first entry which contains the entire JSON string to be deserialize
         list_of_objects = json.loads(listings[0])
-
-        # Sanity check to verify we are loading the number of row we created used the fake data tool
-        # print("\n Number of rows to be written into Spanner: " + str(len(list_of_objects)) + "\n")
-
         # extract values from dictionary objects in the list then convert each item to a tuple and store all items in a list
         list_to_load = [tuple(d.values()) for d in list_of_objects]
-
         # Sort id key in batches before load. further increase speed by using Threads.
         sorted_list = sorted(list_to_load, key=lambda k: k[0])
-
         return sorted_list
 
-
-    # Divide sorted_list into batches of lists of 66 rows each and pass each of those into multiprocessor pool.map(
-    # batching(),sorted_list)
-    def chunks(l, n):
-        """Yield successive n-sized chunks from l."""
-        for i in range(0, len(l), n):
-            yield l[i:i + n]
-
-    batch_lists = list(chunks(get_sorted_list(), 66))
-
     def write_to_spanner(self, batches):
-        with database.batch() as batch:
+        with self.database.batch() as batch:
             batch.insert_or_update(
                 table='LISTINGS',
                 columns=self.columns,
                 values=batches)
 
-    def multithreaded_spanner_write(self):
-        print("Number of sorted lists to be written to Spanner: ", len(self.batch_lists))
-        t1 = time.time()
-        # make the Pool of workers
-        p = ThreadPool(4)
-        result = p.map(self.write_to_spanner, self.batch_lists)
-        print(result)
-        p.close()
-        p.join()
-        load_time = "Time taken to load 10,000 rows into Spanner: " + time.time() - t1
-        return load_time
+    def multithreaded_spanner_write(self, batch_lists):
+        print("Number of sorted lists to be written to Spanner: ", len(batch_lists))
+        try:
+            t1 = time.time()
+            # make the Pool of workers
+            p = ThreadPool(4)
+            p.map(self.write_to_spanner, batch_lists)
+            p.close()
+            p.join()
+            load_time = "Time taken to load 10,000 rows into Spanner: " + str(time.time() - t1)
+            return load_time
+        except Exception as e:
+            logging.error(f"Received Error: {e}")
 
     def create_table(self):
         table = SpannerTable()
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from helper_classes.create_spanner_table import CreateSpannerTable
 from helper_classes.generate_fake_data import OutputFile
 from helper_classes.spanner_operations import SpannerOperations
 
-
 # Create a json and csv files with fake data
 # fake_data_json = OutputFile()
 # fake_data_json.create_fakedata_json()
@@ -19,3 +18,18 @@ tables = table.list_tables()
 print("Tables in database: ")
 for row in tables:
     print(row[0])
+
+# Divide sorted_list into batches of lists of 66 rows each
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+sorted_list = table.get_sorted_list()
+batches = list(chunks(sorted_list, 66))
+
+# Use multithreading to write batches of 66 rows into a Spanner table in a timely fashion
+# This approach takes on average 24 seconds to load 10,000 rows into a Spanner table.
+result = table.multithreaded_spanner_write(batches)
+print(result)
+
